41e019669843ddc6bb382a11e3ef237cf8c43741
jnoggle <justin.noggle@gmail.com>
2016-11-12 02:29:17 -0600

    Add user accounts

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,14 +1,27 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
     tasks() {
-        // Show newest tasks at the top
-        return Tasks.find({}, {sort: {createdAt: -1} });
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+            // If hide completed is checked, filter tasks
+            return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, { sort: { createdAt: -1} });
+    },
+    incompleteCount() {
+        return Tasks.find({ checked: { $ne: true } }).count();
     },
 });
 
@@ -29,5 +42,8 @@ Template.body.events({
 
         // clear form
         target.text.value = '';
+    },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
     },
 });
